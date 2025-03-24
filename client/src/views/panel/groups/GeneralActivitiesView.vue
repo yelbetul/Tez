adf6a3062acd27bebe041a21ffce012a19e2c9fc
Hasan Göksel <hgoksel19@posta.pau.edu.tr>
2025-03-24 10:17:47 +0300

    Update: General and Special Activities API's Integration

--- client/src/views/panel/groups/GeneralActivitiesView.vue
+++ client/src/views/panel/groups/GeneralActivitiesView.vue
@@ -15,89 +15,130 @@
                     <td style="width: 10%;"></td>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Lorem.</td>
-                        <td>Lorem.</td>
-                        <td>Lorem.</td>
-                        <td>Lorem.</td>
-                        <td>Lorem.</td>
+                    <tr v-for="item in general_activities" :key="item.id">
+                        <td>{{ item.general_activity_code }}</td>
+                        <td>{{ item.group_code }}</td>
+                        <td>{{ item.group_name }}</td>
+                        <td>{{ item.sub_group_code }}</td>
+                        <td>{{ item.sub_group_name }}</td>
                         <td>
-                            <i @click="updateGeneralActivity" class="fa-solid fa-pen-to-square"></i>
-                            <i @click="deleteGeneralActivity" class="fa-solid fa-trash-can"></i>
+                            <i @click="updateGeneralActivity(item)" class="fa-solid fa-pen-to-square"></i>
+                            <i @click="deleteGeneralActivity(item)" class="fa-solid fa-trash-can"></i>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
-    <GeneralActivity v-if="modal_visible" :visible="modal_visible" :state="state" @close="closeModal" />
+    <GeneralActivity v-if="modal_visible" :visible="modal_visible" :data="selected_activity" :state="state" @close="closeModal" />
 </template>
 
 <script>
 import GeneralActivity from '@/components/panel/groups/GeneralActivities.vue';
 import PageNavbar from '@/components/panel/PageNavbar.vue';
+import { useAuthStore } from '@/stores/AuthStore';
+import axios from 'axios';
 import Swal from 'sweetalert2';
-    export default {
-        components: {
-            PageNavbar,
-            GeneralActivity
-        },
-        data() {
-            return {
-                navbarData: {
-                    title: 'Genel Faaliyet',
-                    backRoute: '/admin/groups',
-                },
-                state: null,
-                modal_visible: false
-            }
-        },
-        methods:{
-            newGeneralActivity(){
-                this.state = 'new'
-                this.modal_visible = true
+export default {
+    components: {
+        PageNavbar,
+        GeneralActivity
+    },
+    setup() {
+        const authStore = useAuthStore()
+        return { authStore }
+    },
+    data() {
+        return {
+            navbarData: {
+                title: 'Genel Faaliyet',
+                backRoute: '/admin/groups',
             },
-            updateGeneralActivity(){
-                this.state = 'update'
-                this.modal_visible = true
-            },
-            closeModal(){
-                this.state = null
-                this.modal_visible = false
-            },
-            deleteGeneralActivity(){
-                Swal.fire({
-                    title: 'Emin misiniz?',
-                    text: "Bu işlemi geri alamazsınız!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#003049',
-                    cancelButtonColor: '#92140cff',
-                    confirmButtonText: 'Evet, sil!',
-                    cancelButtonText: 'Hayır, iptal et',
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.reload();
-                    }
-                });
-            }
+            state: null,
+            modal_visible: false,
+            general_activities: [],
+            selected_activity: null
         }
+    },
+    methods: {
+        newGeneralActivity() {
+            this.state = 'new'
+            this.modal_visible = true
+        },
+        updateGeneralActivity(item) {
+            this.state = 'update'
+            this.modal_visible = true
+            this.selected_activity = item
+        },
+        closeModal() {
+            this.state = null
+            this.modal_visible = false
+            this.selected_activity = null
+            this.initializeAuth()
+        },
+        deleteGeneralActivity(item) {
+            Swal.fire({
+                title: 'Emin misiniz?',
+                text: "Bu işlemi geri alamazsınız!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#003049',
+                cancelButtonColor: '#92140cff',
+                confirmButtonText: 'Evet, sil!',
+                cancelButtonText: 'Hayır, iptal et',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    axios.delete('https://iskazalarianaliz.com/api/general-activities/delete/' + item.id)
+                        .then(res => {
+                            if (res.data.success) {
+                                this.initializeAuth()
+                                Swal.fire({
+                                    title: 'Silindi!',
+                                    text: 'Genel faaliyet kodu başarıyla silindi.',
+                                    icon: 'success',
+                                });
+                            }
+                        })
+                }
+            });
+        },
+        async initializeAuth() {
+            await this.authStore.fetchAuthData();
+
+            axios.get('https://iskazalarianaliz.com/api/general-activities')
+                .then(res => {
+                    this.general_activities = res.data.data
+                })
+        },
+    },
+    created() {
+        const is_logged_in = localStorage.getItem('is_logged_in') === 'true'
+
+        if (!is_logged_in) {
+            this.$router.push('/admin/login')
+            return
+        }
+
+        this.initializeAuth()
     }
+}
 </script>
 
 <style scoped>
-.page-container{
+.page-container {
     background-color: var(--panel-bg);
     min-height: 100vh;
 }
-.navbar-buttons{
-    width: 100%; 
+
+.navbar-buttons {
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     margin-top: 1%;
 }
-.navbar-buttons span{
+
+.navbar-buttons span {
     width: 20%;
     border: 1px solid var(--main-color);
     display: flex;
@@ -108,19 +149,23 @@ import Swal from 'sweetalert2';
     cursor: pointer;
     transition: all ease .3s;
 }
-.navbar-buttons span i{
+
+.navbar-buttons span i {
     margin-right: 10px;
 }
-.navbar-buttons span:hover{
+
+.navbar-buttons span:hover {
     background-color: var(--main-color);
     color: var(--second-color);
 }
-.table-container{
+
+.table-container {
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 }
+
 table {
     margin-top: 2%;
     width: 90%;
@@ -142,20 +187,27 @@ td {
     height: 50px;
     vertical-align: middle;
 }
-tr:hover{
+
+tr:hover {
     background-color: #f5e7cd;
 }
+
 tbody tr td:last-child {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+    text-align: center;
+    vertical-align: middle;
 }
-tbody tr td:last-child i{
+
+tbody tr td:last-child i {
     font-size: 1.2rem;
     cursor: pointer;
     color: var(--main-color);
 }
-tbody tr td:last-child i:hover{
+
+tbody tr td:last-child i:hover {
     color: var(--penn-red);
+}
+
+.fa-pen-to-square {
+    margin-right: 10px;
 }
 </style>
